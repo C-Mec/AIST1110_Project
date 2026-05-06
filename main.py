dcf7cc18db7730a76db2adb4ca39c9a209c3e1a5
Home <sy10426@syss.edu.hk>
2026-05-06 08:43:10 +0800

    Added Resizing Window

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ ai2 = ui.Player("TEMP2")
 
 players = [player, ai1, ai2]
 
+# Create a resizable window
+main_screen = pygame.display.set_mode(config.screen_dimension, pygame.RESIZABLE)
+manager.init(main_screen)
 
 # Pass manager and player to Grid_Surface
 jeopardy_grid = ui.Grid_Surface(Vec2(1000, 600), Vec2(140, 60), Vec2(6, 5))
@@ -39,6 +42,7 @@ manager.add_surface(jeopardy_grid)
 
 score_overlay = ui.ScoreOverlay(players)
 manager.add_surface(score_overlay)
+
 
 running = True
 while running:
@@ -48,6 +52,31 @@ while running:
         # pygame.QUIT event means the user clicked X to close your window
         if event.type == pygame.QUIT:
             running = False
+        
+        # Handle window resize
+        if event.type == pygame.VIDEORESIZE:
+            # Update screen dimension
+            config.screen_dimension = (event.w, event.h)
+            main_screen = pygame.display.set_mode(config.screen_dimension, pygame.RESIZABLE)
+            manager.init(main_screen)
+
+            # Recompute grid with 1/12 buffer
+            screen_w, screen_h = config.screen_dimension
+            buffer_w = screen_w // 12
+            buffer_h = screen_h // 12
+            grid_w = screen_w - 2 * buffer_w
+            grid_h = screen_h - 2 * buffer_h
+            grid_pos = Vec2(buffer_w, buffer_h)
+
+            # Replace old grid with new resized one
+            manager.layers = [s for s in manager.layers if not isinstance(s, ui.Grid_Surface)]
+            jeopardy_grid = ui.Grid_Surface(Vec2(grid_w, grid_h), grid_pos, Vec2(6, 5))
+            manager.add_surface(jeopardy_grid)
+
+            # Re-add score overlay in top-right
+            manager.layers = [s for s in manager.layers if not isinstance(s, ui.ScoreOverlay)]
+            score_overlay = ui.ScoreOverlay(players)
+            manager.add_surface(score_overlay)
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = Vec2(event.pos)
